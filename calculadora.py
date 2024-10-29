7a5ddbd0c384322792548eb87f0c88eff62aba9f
Camila-flow <a13652@esars.pt>
2024-10-29 09:20:24 +0000

    calculadora.py com funcoes de soma, subtracao, multiplicacao e divisao

--- calculadora.py
+++ calculadora.py
@@ -20,3 +20,27 @@ def calculadora ():
     print("resultado: "+ str(result))
 
 calculadora()
+def multiplicar (x,y):
+    return x * y
+
+# Função principal
+def calculadora ():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    result = multiplicar (num1, num2)
+    print("resultado: "+ str(result))
+
+calculadora()
+# Função para dividir
+def dividir (x,y):
+    return x / y
+
+# Função principal
+def calculadora ():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    result = dividir(num1, num2)
+    print("resultado: "+ str(result))
+
+calculadora()
+
