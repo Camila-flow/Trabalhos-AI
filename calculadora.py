855d48768919d9cf896a1611b1262f9079d2b094
Camila-flow <a13652@esars.pt>
2024-10-29 09:18:34 +0000

    calculadora.py com funcoes de soma e subtracao

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,22 @@
+def soma(x,y):
+    return x + y
 
+# Função principal
+def calculadora ():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    result = soma(num1, num2)
+    print("resultado: "+ str(result))
+
+calculadora()
+def subtrair (x,y):
+    return x - y
+
+# Função principal
+def calculadora ():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    result = subtrair(num1, num2)
+    print("resultado: "+ str(result))
+
+calculadora()
